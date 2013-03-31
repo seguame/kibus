21d4f9ad6e638b97d43db25ed234df622d567e21
Miguel Seguame Reyes <seguame@gmail.com>
2013-03-30 21:55:36 -0600

    Añadida licencia GPL3
    Añadida en la cabecera de cada archivo la licencia de la GPL3

--- Kibus/Escenario.cs
+++ Kibus/Escenario.cs
@@ -1,10 +1,32 @@
-﻿using System;
+﻿//  
+//  Escenario.cs
+//  
+//  Author:
+//       Miguel Seguame Reyes <seguame@outlook.com>
+// 
+//  Copyright (c) 2013 Miguel Seguame Reyes
+// 
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+// 
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+// 
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+using System;
 using Tao.Sdl;
 
 namespace Kibus
 {
 	public class Escenario
 	{
+		
 		private Nivel nivel;
 		
 		public Escenario(Menu.Opcion opcion)
@@ -14,6 +36,10 @@ namespace Kibus
 				case Menu.Opcion.PRACTICA_1:
 					nivel = new Nivel1();
 					break;
+				
+				case Menu.Opcion.PRACTICA_2:
+					break;
+				
 				case Menu.Opcion.CREAR_NIVEL:
 					nivel = new EditorNivel();
 					break;
