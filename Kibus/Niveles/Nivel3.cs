6bb847e889fe1add7b293c6e92e750e51e3f85c5
Miguel Seguame Reyes <seguame@gmail.com>
2013-05-03 00:47:07 -0500

    Se añade la base del nivel 3

---
+++ Kibus/Niveles/Nivel3.cs
@@ -0,0 +1,66 @@
+//  
+//  Nivel3.cs
+//  
+//  Author:
+//       Miguel Seguame Reyes <seguame@outlook.com>
+// 
+//  Copyright (c) 2013 Miguel Seguame Reyes
+// 
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+// 
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+// 
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+using System;
+using Graficos;
+
+
+namespace Niveles
+{
+	internal class Nivel3 : Nivel
+	{
+		public Nivel3 ()
+		{
+			new CargadorMapas().SeleccionarArchivo();
+		}
+		
+		public override void Iniciar()
+		{
+			int[][] mapa = CargadorMapas.Mapa;
+			
+			//TODO:
+			//Quitar de todos el tamaño fijo
+			for(int i = 0; i < 20; i++)
+			{
+				for(int j = 0; j < 20; j++)
+				{
+					if(mapa[i][j] == EditorMapas.CASA)
+					{
+						Console.WriteLine("Casa en {0},{1}", i, j);
+						this.casa = sprites[i,j] = new Sprite("Assets/GFX/casini.png");
+					}
+					else if(mapa[i][j] != 0)
+					{
+						Console.WriteLine("{2} en {0},{1}", i, j, mapa[i][j]);
+						sprites[i,j] = new Sprite("Assets/GFX/"+mapa[i][j]+".png");
+					}
+					
+					if(sprites[i,j] != null)
+					{
+						sprites[i,j].Mover((short)(i * sprites[i,j].Ancho),(short) (j * sprites[i,j].Alto));
+					}
+				}
+			}
+			
+			PosicionarKibus();
+		}
+	}
+}
+
