bb9805070d9fb28f568cd56edac0829edbfd5bb2
Miguel Seguame Reyes <seguame@gmail.com>
2013-06-09 16:18:47 -0500

    Cambio menor, solo para lograr tener un inverso de direcciones en caso de que sea requerido

--- Kibus/Utileria.cs
+++ Kibus/Utileria.cs
@@ -28,13 +28,13 @@ namespace Utileria
 	public enum Direccion
 	{
 		ARRIBA,
-		ABAJO,
 		IZQUIERDA,
-		DERECHA,
 		ARRIBA_IZQ,
 		ARRIBA_DER,
 		ABAJO_IZQ,
 		ABAJO_DER,
+		DERECHA,
+		ABAJO,
 		MISINGNO
 	};
 	
