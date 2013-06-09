b13aa78ca9d59e2e27b86e7bd2c0d07c17c4188d
Miguel Seguame Reyes <seguame@gmail.com>
2013-06-08 23:20:07 -0500

    Completacion y calculos requeridos para el entrenamiento

--- Kibus/Niveles/Nivel4.cs
+++ Kibus/Niveles/Nivel4.cs
@@ -32,15 +32,9 @@ namespace Niveles
 		private int minimo = Int32.MaxValue;
 		private int maximo = Int32.MinValue;
 		private int mediaHistorica;
-		private int mediaHistoricaAnterior;
-		private int castigo;
-		//private ArregloNodosMapa nodosMapa;
 		private Nodo[] arregloNodosTotal;
 		
 		private Nodo[] nodosVisitados;
-		private Conexion[] conexionesVisitadas;
-		
-		private bool pimeraVuelta = true;
 		
 		
 		private int[][] mapa;
@@ -50,7 +44,6 @@ namespace Niveles
 		{
 			new CargadorMapas().SeleccionarArchivo();
 			
-			//nodosMapa = new ArregloNodosMapa();
 			arregloNodosTotal = new Nodo[20*20];
 		}
 		
@@ -85,21 +78,20 @@ namespace Niveles
 			}
 			
 			PosicionarKibus();
-			
-			//Setear los 2 nodos iniciales, origen y destino
-			arregloNodosTotal[kibus.OnToyY + kibus.OnToyX * 20] = new Nodo();
-			arregloNodosTotal[casa.OnToyY + casa.OnToyX * 20] = new Nodo();
-			
 			//El entrenamiento puesun
 			ConfeccionarCamino();
+			
+			foreach(Nodo n in arregloNodosTotal)
+			{
+				if(n != null)
+					Console.WriteLine(n.ToString());
+			}
 		}
 		
 		private void ConfeccionarCamino()
 		{
-			//Volver a iniciar los valores;
-			nodosVisitados = new Nodo[20*20];
-			conexionesVisitadas = new Conexion[20*20];
-			Nodo.CantidadNodosVisitados = 0;
+			int repeticiones = 1000;
+			int castigo;
 			
 			Direccion direccion;
 			Random random = new Random(System.DateTime.Now.Millisecond);
@@ -107,80 +99,141 @@ namespace Niveles
 			Console.WriteLine("CASA: {0},{1}", casa.OnToyX,casa.OnToyY);
 			Console.WriteLine("KIBU: {0},{1}", kibus.OnToyX,kibus.OnToyY);
 			
-			while(kibus.OnToyX != casa.OnToyX || kibus.OnToyY != casa.OnToyY)
+			int origen = kibus.OnToyY + kibus.OnToyX * 20;
+			int destino = casa.OnToyY + casa.OnToyX * 20;
+			//Setear los 2 nodos iniciales, origen y destino
+			
+			Sdl.SDL_Rect rectanguloOrigen = kibus.GetRectangulo();
+			
+			while(repeticiones-->0)
 			{
-				modoGrafico = !Hardware.TeclaPulsada(Sdl.SDLK_g);
+				Console.WriteLine(repeticiones);
+				//Volver a iniciar los valores;
+				nodosVisitados = new Nodo[20*20];
+				Nodo.CantidadNodosVisitados = 0;
+				Conexion.conexionesUsadas = 0;
 				
-				if(modoGrafico)
-				{
-					DibujarTodo();
-					Hardware.EscribirTexto("Entrenando", 641, 10); 
-					Hardware.RefrescarPantalla();
-				}
+				nodosVisitados[destino] = new Nodo();
+				nodosVisitados[origen] = new Nodo();
 				
-				do
+				kibus.Mover(rectanguloOrigen);
+				
+				while(kibus.OnToyX != casa.OnToyX || kibus.OnToyY != casa.OnToyY)
 				{
+					modoGrafico = Hardware.TeclaPulsada(Sdl.SDLK_g);
 					
-					direccion = (Direccion)random.Next(0, (int)Direccion.MISINGNO);
+					if(modoGrafico)
+					{
+						DibujarTodo();
+						Hardware.EscribirTexto("Entrenando", 641, 10); 
+						Hardware.RefrescarPantalla();
+					}
 					
-				}while(!IntentarMover(kibus, direccion, false));
-				
-				int anterior = kibus.OnToyY + kibus.OnToyX * 20;
-				
-				Conexion destino = new Conexion();
-				
-				kibus.Mover(direccion);
-				
-				int actual = kibus.OnToyY + kibus.OnToyX * 20;
-				
-				if(arregloNodosTotal[actual] == null)
-				{
-					arregloNodosTotal[actual] = new Nodo();
+					do
+					{
+						
+						direccion = (Direccion)random.Next(0, (int)Direccion.MISINGNO);
+						
+					}while(!IntentarMover(kibus, direccion, false));
+					
+					int anterior = kibus.OnToyY + kibus.OnToyX * 20;
+					
+					kibus.Mover(direccion);
+					
+					int actual = kibus.OnToyY + kibus.OnToyX * 20;
+					
+					if(nodosVisitados[actual] == null)
+					{
+						nodosVisitados[actual] = new Nodo();
+					}
+					
+					Conexion cnxDestino;
+					
+					if(nodosVisitados[anterior].conexion[(int)direccion] == null)
+					{
+						cnxDestino = new Conexion();
+					}
+					else
+					{
+						cnxDestino = nodosVisitados[anterior].conexion[(int)direccion];
+					}
+					                                     
+					cnxDestino.NodoConexion = nodosVisitados[actual];
+					
+					//Verificacion chafa a falta de la instruccion Assert.
+					if(nodosVisitados[anterior] == null)
+					{
+						throw new Exception("Esto no debe pasar");
+					}
+					
+					
+					nodosVisitados[anterior].conexion[(int)direccion] = cnxDestino;
+					
+					
+					
+					if(modoGrafico)
+					{
+						Hardware.Pausar(10);
+					}
 				}
 				
-				destino.NodoConexion = arregloNodosTotal[actual];
+				//Console.WriteLine("CASA: {0},{1}", casa.OnToyX,casa.OnToyY);
+				//Console.WriteLine("KIBU: {0},{1}", kibus.OnToyX,kibus.OnToyY);
 				
-				//Verificacion chafa a falta de la instruccion Assert.
-				if(arregloNodosTotal[anterior] == null)
+				
+				
+				//Seteo de los valores minimos y máximos
+				if(Conexion.conexionesUsadas > maximo) maximo = Conexion.conexionesUsadas;
+				if(Conexion.conexionesUsadas < minimo) minimo = Conexion.conexionesUsadas;
+				
+				mediaHistorica = (maximo + minimo)/2;
+				
+				
+				castigo = /*Math.Abs(*/Conexion.conexionesUsadas - mediaHistorica/*)*/;
+				
+				
+				Console.WriteLine("Maximo :" + maximo);
+				Console.WriteLine("Minimo :" + minimo);
+				Console.WriteLine("Media  :" + mediaHistorica);
+				Console.WriteLine("Conexiones: " + Conexion.conexionesUsadas);
+				Console.WriteLine("Castigo: " + castigo);
+				Console.WriteLine(".------------------.");
+				actualizarMundoValorifico(nodosVisitados, castigo);
+			}
+		}
+		
+		private void actualizarMundoValorifico(Nodo[] nodosVisitados, int castigo)
+		{
+			for(int i = 0; i < nodosVisitados.Length; i++)
+			{
+				if(nodosVisitados[i] != null)
 				{
-					throw new Exception("Esto no debe pasar");
-					//arregloNodosTotal[anterior] = new Nodo();
-				}
-				
-				
-				arregloNodosTotal[anterior].conexion = destino;
-				
-				
-				
-				if(modoGrafico)
-				{
-					Hardware.Pausar(2);
-				}
-				else
-				{
-					//Hardware.Pausar(2);
+					if(arregloNodosTotal[i] == null)
+					{
+						arregloNodosTotal[i] = nodosVisitados[i];
+					}
+					else
+					{
+						//Checar las 8 conexiones del nodo y ver que fueron usadas, para aplicarles el castigop puesun.
+						for(int j = 0; j < nodosVisitados[i].conexion.Length; j++)
+						{
+							
+							if(nodosVisitados[i].conexion[j] != null) //Si la conexion se generó se verá actualizado su valor en el mapa
+							{
+								if(arregloNodosTotal[i].conexion[j] == null) //si esto ocurre, se asignal la conexion tal cual, pues es nueva
+								{
+									arregloNodosTotal[i].conexion[j] = nodosVisitados[i].conexion[j];
+								}
+								else //se actualizara el valor dependiendo del castigo
+								{
+									arregloNodosTotal[i].conexion[j].Peso += castigo;
+								}
+							}
+							//else, si no se utilizo la conexion, no tenemos que actualizar su situacion en el mapa
+						}
+					}
 				}
 			}
-			
-			Console.WriteLine("CASA: {0},{1}", casa.OnToyX,casa.OnToyY);
-			Console.WriteLine("KIBU: {0},{1}", kibus.OnToyX,kibus.OnToyY);
-			
-			Console.WriteLine("Nodos visitados: {0}", Nodo.CantidadNodosVisitados);
-			
-			
-			//Seteo de los valores minimos y máximos
-			if(Nodo.CantidadNodosVisitados > maximo) maximo = Nodo.CantidadNodosVisitados;
-			if(Nodo.CantidadNodosVisitados < minimo) minimo = Nodo.CantidadNodosVisitados;
-			
-			mediaHistorica = (maximo + minimo)/2;
-			
-			if(pimeraVuelta)
-			{
-				mediaHistoricaAnterior = mediaHistorica;
-				pimeraVuelta = false;
-			}
-			
-			castigo = Math.Abs(mediaHistorica - mediaHistoricaAnterior);
 		}
 	}
 }
